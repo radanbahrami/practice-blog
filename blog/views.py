fbbecacf0db9c1fd6dc3939a2dad2e85c8c843f9
Radan Bahrami <201373807+radanbahrami@users.noreply.github.com>
2025-09-06 18:44:43 +0330

    Add API views for post list and post detail

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.http import HttpResponse
 from .models import Post
+from .serializers import PostSerializer
 
 def post_titles(request):
     posts = Post.objects.all()
     output = "<br>".join([p.title for p in posts])
     return HttpResponse(output)
+
+@api_view(['GET'])
+def post_list(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def post_detail(request, pk):
+    post = Post.objects.get(id=pk)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
