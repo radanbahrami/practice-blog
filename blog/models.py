50a774f9fc0221efc24ece0ff937502224c39dee
Radan Bahrami <201373807+radanbahrami@users.noreply.github.com>
2025-09-06 14:09:47 +0330

    Define Author and Post models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
