cccd22f547ea313ad62e10a297b65dc1bedc8781
Radan Bahrami <201373807+radanbahrami@users.noreply.github.com>
2025-09-06 18:57:42 +0330

    Add API routes for post list and post detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import post_titles
+from .views import post_titles, post_list, post_detail
 
 urlpatterns = [
-    path("post-titles/", post_titles, name="post_titles")
+    path("post-titles/", post_titles, name="post_titles"),
+    path("api/posts/", post_list, name="post_list"),
+    path("api/posts/<int:pk>/", post_detail, name="post_detail"),
 ]
